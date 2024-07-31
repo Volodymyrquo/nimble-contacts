95972b1d0ccc9c7f66f4fd827d9b25024e0e89f1
Volodymyr Basok <volodymyrquo@gmail.com>
2024-07-31 13:28:27 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { Contact, Home } from './pages';
+
 
 function App() {
-
-  return (
-    <>
-     <div className="text-3xl font-bold underline">
-      Hello world!
-     </div>
-    </>
-  )
+    return (
+        <main>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact/:id" element={<Contact />} />
+                </Routes>
+            </Router>
+        </main>
+    );
 }
 
-export default App
+export default App;
